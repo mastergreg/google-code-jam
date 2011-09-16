5be42e0220dafdc58675b9daa0f663d5b86b5a8e
mastergreg <gregliras@gmail.com>
2011-09-16 04:41:01 +0300

    not correct

--- 2011/C/Python/solve.py
+++ 2011/C/Python/solve.py
@@ -7,7 +7,7 @@
 #
 #* Creation Date : 16-09-2011
 #
-#* Last Modified : Fri 16 Sep 2011 04:27:58 AM EEST
+#* Last Modified : Fri 16 Sep 2011 04:39:33 AM EEST
 #
 #* Created By : Greg Liras <gregliras@gmail.com>
 #
@@ -26,7 +26,7 @@ def playMaxTurns(cards):
     if t > maxT:
       maxT = t
       maxI = i
-  if maxT = 0: # if aint getting no turns for me gimme score
+  if maxT == 0: # if aint getting no turns for me gimme score
     return playMaxScore(cards)
   return maxI
 def playMaxCards(cards):
@@ -56,7 +56,7 @@ def playGetAll(rounds,hand,deck,handList,deckList):
 
 
 def pick_card(rds,cAh,cId,cAhL,cIdL):
-  if cAh == 0:
+  if cAh == 1:
     return 0
   if cId == 0:
     return playMaxTurns(cAhL)
@@ -74,18 +74,26 @@ for i in range(cases):
   cardsAthand = int(f.readline())
   card_at_hand_list=[]
   for j in range(cardsAthand):
-    card_at_hand_list.append(tuple(f.readline().split()))
+    card_at_hand_list.append(tuple(map (int, f.readline().split())))
   cardsIndeck = int(f.readline())
   card_in_deck_list=[]
   for j in range(cardsIndeck):
-    card_in_deck_list.append(tuple(f.readline().split()))
+    card_in_deck_list.append(tuple(map (int, f.readline().split())))
   rounds_left = 1
   score = 0
-  while rounds_left>0 and cardsAthand > 0:
+ 
+  while rounds_left > 0 and cardsAthand > 0:
     card_number = pick_card(rounds_left,cardsAthand,cardsIndeck,card_at_hand_list,card_in_deck_list)
-    print card_at_hand_list[card_number]
-    print card_in_deck_list
+    (c,s,t) = card_at_hand_list.pop(card_number)
+    card_at_hand_list += card_in_deck_list[:c]
+    card_in_deck_list = card_in_deck_list[c:]
+    cardsAthand = len(card_at_hand_list)
+    score+=s
+    rounds_left = t-1
 
 
-  answer = ""
+
+
+
+  answer = str(score)
   print "Case #"+str(i+1)+": "+answer
